3112d6ed8f2f24b1c5fd2eede37058ba241bee7a
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2023-07-13 16:53:11 -0300

    Criação do novos métodos com came case

--- Dao/ClienteDao.php
+++ Dao/ClienteDao.php
@@ -1,33 +1,33 @@
 <?php
-    require "../Conexao.php";
     require "../Class/Cliente.php";
-
     class ClienteDao {
         private $conexao;
-        private $table = "clientes";
+        private $tabela = "clientes";
         
         public function __construct() {
             $this->conexao = new Conexao();
         }
         
         public function ehCliente ($cliente = null){
-            $clientes = $this->conexao->select($this->table, ["nome, email, telefone"], ["email"=>"clemerson@gmail.com"]);
-            
-            echo "<br><br>Clientes:" ;
-            print_r($clientes);
-            
-            
-            return false;
+            $clientes = $this->conexao->select($this->tabela, ["nome, email, telefone"], ["email"=>$cliente->getEmail()]);
+            return count($clientes) > 0;
         }
 
-        public function inserir ($cliente=null){
-            return true;
-            //return $this->conexao->insert($this->table, $cliente);
-        }
-       
-        public function listar_clientes (){
-            return $this->conexao->select($this->table, ["*"]);
+        public function inserir ($cliente = null){
+            return $this->conexao->insert($this->tabela, $cliente);
         }
 
+        public function getCliente ($email = ""){
+            $clientesArray = $this->conexao->select($this->tabela, ["*"], ["email"=>$email]);
+            $objCliente = new Cliente();
+            $objCliente->setNome($clientesArray[0]['nome']);
+            $objCliente->setEmail($clientesArray[0]['email']);
+            $objCliente->setTelefone($clientesArray[0]['telefone']);
+            $objCliente->setId($clientesArray[0]['id']);
+            return $objCliente;
+        }
 
+        public function listarClientes (){
+            return $this->conexao->select($this->tabela, ["*"], ["1"=>"1"]);
+        }
     }
